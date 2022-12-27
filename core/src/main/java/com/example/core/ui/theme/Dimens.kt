2824a45829a5354f7ec24ee3d4800875d1868e8b
Tilen Prepeluh <tpr@shpock.com>
2022-12-27 06:47:47 +0100

    core theme

---
+++ core/src/main/java/com/example/core/ui/theme/Dimens.kt
@@ -0,0 +1,18 @@
+package com.example.core.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val spacing4 = 4.dp
+    val spacing8 = 8.dp
+    val spacing12 = 12.dp
+    val spacing16 = 16.dp
+    val spacing56 = 56.dp
+}
+
+object Sizes {
+    val smallIcon = 24.dp
+    val mediumIcon = 48.dp
+    val smallImage = 200.dp
+    val mediumImage = 400.dp
+}
